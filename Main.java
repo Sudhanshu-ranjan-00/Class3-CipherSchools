cbcf70e4f8ca3df6c73926670f8ae943a2fd4d10
Sudhanshu Ranjan <rajaashujha@gmail.com>
2023-06-24 20:24:08 +0530

    Class3-CipherSchools

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        System.out.println("Java features : ");
+        System.out.println("Simple");
+        System.out.println("Robust");
+        System.out.println("Portable");
+        System.out.println("Object oriented");
+        System.out.println("Platform independent");
+        System.out.println("Secure");
+        System.out.println("High performance");
+        System.out.println("Distributed");
+        System.out.println("Secure");
+        System.out.println("Multithreaded");
+        System.out.println("Architectural neutral");
+    }
+}
